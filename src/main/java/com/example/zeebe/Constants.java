9823675daab0f6599f75d96ce6ac52e72a4a6466
Atul Kulkarni <atul.kulkarni@metrosystems.net>
2019-07-31 15:28:12 +0200

    removed compile time dependency with zeebe-simple-java-client

---
+++ src/main/java/com/example/zeebe/Constants.java
@@ -0,0 +1,13 @@
+package com.example.zeebe;
+
+public class Constants {
+
+	public static final String ORDER_ID = "4";
+	
+	public static final int ORDER_VALUE_INT_WITHOUT_INSURANCE = 99;
+	
+	public static final int ORDER_VALUE_INT_WITH_INSURANCE = 101;
+	
+	public static final String ORDER_VALUE_STRING = "111";
+	
+}
